e36fc67eb738e19f5b2a5653a8be194bcff882cc
Minkee Sung <minkeesung@gmail.com>
2017-06-21 17:20:22 -0400

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,40 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def login
+    @user = User.new
+  end
+
+  def logout
+    session.clear
+    redirect_to '/'
+  end
+
+  def create_session
+
+    @user = User.find_by(email: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      @user = User.new
+      @user.errors.add(:invalid, "Email or Password")
+      render "login"
     end
   end
 

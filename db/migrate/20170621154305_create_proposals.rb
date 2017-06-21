ba2d91f277fb03f8766532c50228c9348fca3ac2
Minkee Sung <minkeesung@gmail.com>
2017-06-21 18:25:00 -0400

    worked on proposals

--- db/migrate/20170621154305_create_proposals.rb
+++ db/migrate/20170621154305_create_proposals.rb
@@ -1,11 +1,11 @@
 class CreateProposals < ActiveRecord::Migration[5.1]
   def change
     create_table :proposals do |t|
-      t.string :name
-      t.string :summary
-      t.string :state
-      t.string :hypothesis
-      t.references :proposer
+      t.string :name, null: false
+      t.string :summary, null: false
+      t.string :state, null: false
+      t.string :hypothesis, null: false
+      t.references :proposer, null: false
 
       t.timestamps
     end
